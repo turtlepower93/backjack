d9626607a0cd805804bc592355d76313e0cc6907
Richard Williams <richelliswilliams@gmail.com>
2020-11-12 19:18:26 -0800

    fix several bugs

---
+++ README.md
@@ -0,0 +1,54 @@
+# BlackJack!
+
+The goal of this game is to beat the dealer!
+
+(include screenshot)
+
+
+## Technologies Used
+
+HTML
+CSS
+Javascript
+
+Here is the link:  https://turtlepower93.github.io/blackjack/
+
+# GamePlay
+
+## BettingStage
+
+Player places their bet at the beginning of the game before the hands are dealt.
+
+To bet, click on the chips.
+
+Your total wallet is displayed in the bottom left of the screen.
+
+You can see the current Pot in the middle of the screen.
+
+Once you are done betting, Hit Deal!
+
+## Gameplay
+
+In order to win, your hands value must be higher than the dealer.
+
+You can : Hit or Stay.
+
+Hitting adds an additional card to your hand. Be careful, If you go over 21 you lose!
+
+Staying starts the dealers turn. Once the dealer has finished his turn the game is over!
+
+If you get the same value = Tie
+
+# Next Steps
+
+I intend to add the following features:
+
+Add the ability to Split
+
+Add the ability to insure your hand
+
+If you don't have enough to bet a certain amount, then remove that chip from the table.
+
+Add media queries that will allow the game to be played on all devices
+
+Make it prettier! (better background, change the fonts around, different color chips, animations, sound etc.)
--- script/main.js
+++ script/main.js
@@ -30,6 +30,7 @@ const dealerTotalEl = document.getElementById('dealer-total');
 const playerTotalEl = document.getElementById('player-total');
 const playerWalletEl = document.getElementById('current-wallet');
 const potTotalEl = document.getElementById('pot-total')
+const winDisplayEl = document.querySelector('p')
 
 
 //EVENT LISTENERS
@@ -43,10 +44,7 @@ dealBut.addEventListener('click', init);
 
 init();
 
-function gameLoop() {
-    dealHands();
-    render();
-}
+
 
 //Deal beginning hands
 function dealHands() {
@@ -56,8 +54,7 @@ function dealHands() {
     while(dealerHand.length <= 1) {
         pullCard(dealerHand,dealerTotal,0);
     }
-    console.log('player Hand: '+ playerHand);
-    console.log('dealerHand: '+ dealerHand);
+
 }
 
 //Pull random card from currentDeck and place it in hand.
@@ -72,20 +69,12 @@ function pullCard(currentHand,currentTotal,who) {
 
 //adds up the value of hand, helper from pullcard
 function updateHandTotal(currentHand,currentTotal,who) {
-
-
-    
-
     currentHand.forEach(function (ele) {
-
-        let num = ele.slice(1,3);
-        
+        let num = ele.slice(1,3);  
         if((num === 'K')||(num === 'Q')||(num === 'J')) {
             num = 10;
         }
-
         if(num === 'A') {
-            debugger
             if(currentTotal < 10) {
                 num = 11;
             } else {
@@ -93,19 +82,13 @@ function updateHandTotal(currentHand,currentTotal,who) {
             }
         }
         num = parseInt(num)
-        currentTotal = num;
-        
+        currentTotal = num;  
     })
 
-    
-
-    
-    
-
+    //Update Totals
     if(who) {
         playerTotal += currentTotal;
     }
-      
     if( 0 === who ) {
         dealerTotal += currentTotal;
         dealerTotal += poppedTotal;
@@ -116,28 +99,22 @@ function updateHandTotal(currentHand,currentTotal,who) {
         poppedTotal = currentTotal;
         onlyCountOnce = false;
     }
-    
-
-    console.log('playerTotal is' + playerTotal);
-    console.log('dealerTotal is' + dealerTotal);
-    console.log(currentHand);
 }
 
+//Bet handler
 function handleBet(evt) {
-
     selectedChip = evt.target.id
     if('chip-container' === selectedChip) {
         return;
     }
     selectedChip = selectedChip.slice(2,5);
     selectedChip = parseInt(selectedChip);
-    if(wallet < selectedChip) return console.log('YOUBROKEFOOL');
+    if(wallet < selectedChip) return;
     //move chip to pot icebox
     wallet -= selectedChip;
     pot.push(selectedChip);
     countPot();
     render();
-
 }
 
 function countPot() {
@@ -145,7 +122,21 @@ function countPot() {
     console.log(potTotal);
 }
 
-
+//Display you win, you lose, tie!
+function renderWinState(state) {
+    if(state === 0) {
+        winDisplayEl.textContent = 'You Lose!';
+    }
+    if(state === 1) {
+        winDisplayEl.textContent = "Winner Winner Chicken Dinner!";
+    }
+    if(state === -1) {
+        winDisplayEl.textContent = "";
+    }
+    if(state === 2) {
+        winDisplayEl.textContent = "tie!";
+    }
+}
 
 function handleHit() {
     pullCard(playerHand,playerTotal, 1);
@@ -164,40 +155,41 @@ function handleStay() {
 }
 
 function dealerPlays() {
-
     while(playerTotal >= dealerTotal) {
         if (dealerTotal >= 17) {
             break;
         }
         pullCard(dealerHand,dealerTotal, 0);
-        console.log(dealerHand);
-        console.log("dealer hits, new total is:" + dealerTotal)
     }
-    console.log(dealerTotal)
 }
 
+//Logic to determine who is the winner.
 function whoWon() {
-    
     if(playerTotal > 21) {
+        renderWinState(0);
     }
     
     if (dealerTotal > 21) {
+        renderWinState(1);
         wallet += (potTotal*2);
     }
 
     if((dealerTotal < 21 && playerTotal <= 21) && playerTotal > dealerTotal) {
+        renderWinState(1);
         wallet += (potTotal*2);
     }
     if((dealerTotal <= 21 && playerTotal < 21) && dealerTotal > playerTotal) {
+        renderWinState(0);
     } 
 
     if(playerTotal === 21 && dealerTotal === 21) {
-        console.log('Push')
+        renderWinState(2);
         wallet += potTotal;
     }
-    renderDealHandButton(1) 
+    renderDealHandButton(1);
     render();
 }
+
 
 function renderDealHandButton(state) {
     if(state === 0) {
@@ -212,6 +204,7 @@ function renderDealHandButton(state) {
     }
 }
 
+//Renders the values in the middle of screen.
 function renderValue() {
     dealerTotalEl.innerHTML = `<div id="dealer-total">${dealerTotal}</div>`;
     playerTotalEl.innerHTML = `<div id="player-total">${playerTotal}</div>`;
@@ -219,6 +212,7 @@ function renderValue() {
     playerWalletEl.innerHTML = `<div id="player-total">Wallet : $${wallet}</div>`;
 }
 
+//Stops the player from being able to bet during the game.
 function renderDisableBetting(state){
     if(state === 1) {
         chipButs.style.pointerEvents = 'all';
@@ -228,16 +222,8 @@ function renderDisableBetting(state){
     }
 }
 
-function renderMoveCards() {
-
-}
-
-function renderMoveChip() {
-
-}
-
+//RendersCards
 function renderCards() {
-
     playerContainer.innerHTML = ` `;
     playerHand.forEach(function(ele) {
         playerContainer.insertAdjacentHTML('afterbegin',`<div class="card ${ele}" align-content="flex-end"></div>`);
@@ -247,7 +233,7 @@ function renderCards() {
     dealerHand.forEach(function(ele) {
         dealerContainer.insertAdjacentHTML('afterbegin',`<div class="card ${ele}" align-content="flex-end"></div>`);
     })
-    // debugger
+
     if(dealerHand.length === 2) {
         dealerContainer.innerHTML = ` `;
         dealerContainer.insertAdjacentHTML('afterbegin',`<div class="card back" align-content="flex-end"></div>`);
@@ -255,6 +241,7 @@ function renderCards() {
     }        
 }
 
+//Updates the cards, potValue, player and dealer hand values
 function render() {
    renderCards();
    renderValue();
@@ -284,10 +271,12 @@ function init() {
     playerTotal = null;
     dealerHand = [];
     dealerTotal = null;
-    renderDealHandButton(0);
+    renderWinState(-1);
+    renderDealHandButton(1);
     renderDisableBetting(1)
     refreshDeck();
-    gameLoop();
+    dealHands();
+    render();
 }
 
 
